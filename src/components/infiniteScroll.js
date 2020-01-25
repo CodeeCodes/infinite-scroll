59d0c8161617ecc85980a9fbae01bd62050635fd
Codee Palmer <codeejpalmer@gmail.com>
2020-01-25 10:11:10 -0800

    Working on scroll functionality

--- src/components/infiniteScroll.js
+++ src/components/infiniteScroll.js
@@ -8,32 +8,41 @@ export default function InfiniteScroll() {
   //useEffect to fetch data when page is loaded and set the state
   //setting load more data to false
   useEffect(() => {
-    fetchData(loadData);
-    setLoadData(false);
-  }, []);
-
-  //setting element to variable, then checking where it is on the window
-  useEffect(() => {
-    const imageDiv = document.querySelector(".infinite");
-    if (imageDiv.clientHeight <= window.innerHeight && imageDiv.clientHeight) {
-      setLoadData(true);
+    if (loadData === true) {
+      fetchData(loadData);
+      setLoadData(false);
     }
   }, []);
 
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  function handleScroll() {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+      document.documentElement.clientHeight
+    ) {
+      console.log("fetch more");
+      fetchData();
+    }
+  }
+
   //fetching the data, randomizing the data in the response and selecting a random 100 subset
-  const fetchData = fetch => {
-    if (fetch) {
+  const fetchData = () => {
+    if (loadData) {
       axios
         .get("https://jsonplaceholder.typicode.com/photos/")
         .then(response => {
           const arrRan = response.data
             .sort(() => 0.5 - Math.random())
-            .slice(0, 100);
+            .slice(0, 20);
           setState([...state, arrRan]);
         });
     }
   };
-
+  console.log(loadData);
   //mapping through our state and outputting data
   const mappedImages =
     state[0] &&
